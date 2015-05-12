7801d67c6a9886d7802e494a98cba805d7a0e455
Ravi Kiran <raak@hackerstrip.com>
2015-05-12 15:07:28 +0530

    function test

--- fb.php
+++ fb.php
@@ -1,4 +1,9 @@
 <?hh
-echo "hello world\n";
-$a = 1234; // decimal number
-echo $a;
+class MyClass {
+  const int MyConst = 0;
+  private string $x = '';
+  public function increment(int $x): int {
+    $y = $x + 1;
+    return $y;
+  }
+}
